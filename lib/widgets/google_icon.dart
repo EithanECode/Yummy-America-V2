8c0abc2dcb1ba6a1a629ce4129d0624d4b1f89a5
Eithan <eithanbv006@gmail.com>
2025-07-28 15:03:38 -0400

    Elimina el archivo google_icon.svg y actualiza pubspec.yaml para incluir las nuevas rutas de los activos. Modifica el widget GoogleIcon para cargar el SVG desde la nueva ubicación en assets/icons/.

--- lib/widgets/google_icon.dart
+++ lib/widgets/google_icon.dart
@@ -3,29 +3,16 @@ import 'package:flutter_svg/flutter_svg.dart';
 
 class GoogleIcon extends StatelessWidget {
   final double size;
-  final Color? backgroundColor;
 
-  const GoogleIcon({super.key, this.size = 24.0, this.backgroundColor});
+  const GoogleIcon({super.key, this.size = 24.0});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return SvgPicture.asset(
+      'assets/icons/google_icon.svg',
       width: size,
       height: size,
-      decoration: BoxDecoration(
-        color: backgroundColor ?? Colors.white,
-        borderRadius: BorderRadius.circular(size * 0.1),
-        border: Border.all(color: Colors.grey.shade300, width: 1),
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(size * 0.1),
-        child: SvgPicture.asset(
-          'google_icon.svg',
-          width: size * 0.8,
-          height: size * 0.8,
-          fit: BoxFit.contain,
-        ),
-      ),
+      fit: BoxFit.contain,
     );
   }
 }
